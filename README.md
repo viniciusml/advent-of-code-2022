4f6830b41ddfb047120f1ac85153fc945d0dea57
Vinicius Leal <viniciusmoreira.leal@thetrainline.com>
2022-11-17 15:55:56 +0000

    generate package

---
+++ README.md
@@ -0,0 +1,3 @@
+# advent-of-code
+
+A description of this package.
---
+++ Sources/advent-of-code/advent_of_code.swift
@@ -0,0 +1,6 @@
+public struct advent_of_code {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
