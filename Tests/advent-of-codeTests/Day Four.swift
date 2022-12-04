219b2bfcac10fa75c95addb6ad67c428368e9b7d
Vinicius Leal <viniciusmoreira.leal@thetrainline.com>
2022-12-04 12:25:23 +0000

    Day Four part two

--- Tests/advent-of-codeTests/Day Four.swift
+++ Tests/advent-of-codeTests/Day Four.swift
@@ -37,5 +37,21 @@ final class DayFourTests: XCTestCase {
     
     func testSecondPart() {
         let parsed = Parser.parse(.content(day: "Four"), separator: "\n")
+        
+        let ranges = parsed.map { line in
+            let pairs = line.components(separatedBy: ",")
+            return pairs.map { pair in
+                let initialAndFinal = pair.components(separatedBy: "-")
+                if let first = initialAndFinal.first, let final = initialAndFinal.last, let intFirst = Int(first), let intFinal = Int(final) {
+                    return (intFirst...intFinal)
+                } else { fatalError() }
+            }
+        }
+        
+        let result = ranges.map { range in
+            range[1].overlaps(range[0])
+        }.filter { $0 == true }
+        
+        XCTAssertEqual(result.count, 830)
     }
 }
