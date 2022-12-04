68e6b4e6490d2592386752538daafad2f0284f3d
Vinicius Leal <viniciusmoreira.leal@thetrainline.com>
2022-12-04 12:30:41 +0000

    Refactor

--- Tests/advent-of-codeTests/Day Four.swift
+++ Tests/advent-of-codeTests/Day Four.swift
@@ -18,19 +18,11 @@ final class DayFourTests: XCTestCase {
     func testFirstPart() {
         let parsed = Parser.parse(.content(day: "Four"), separator: "\n")
         
-        let ranges = parsed.map { line in
-            let pairs = line.components(separatedBy: ",")
-            return pairs.map { pair in
-                let initialAndFinal = pair.components(separatedBy: "-")
-                if let first = initialAndFinal.first, let final = initialAndFinal.last, let intFirst = Int(first), let intFinal = Int(final) {
-                    return (intFirst...intFinal)
-                } else { fatalError() }
-            }
-        }
+        let ranges = ranges(from: parsed)
         
-        let result = ranges.map { range in
+        let result = result(from: ranges) { range in
             range[0].embraces(range[1]) || range[1].embraces(range[0])
-        }.filter { $0 == true }
+        }
         
         XCTAssertEqual(result.count, 477)
     }
@@ -38,7 +30,17 @@ final class DayFourTests: XCTestCase {
     func testSecondPart() {
         let parsed = Parser.parse(.content(day: "Four"), separator: "\n")
         
-        let ranges = parsed.map { line in
+        let ranges = ranges(from: parsed)
+        
+        let result = result(from: ranges) { range in
+            range[1].overlaps(range[0])
+        }
+        
+        XCTAssertEqual(result.count, 830)
+    }
+    
+    private func ranges(from input: [String]) -> [[ClosedRange<Int>]] {
+        input.map { line in
             let pairs = line.components(separatedBy: ",")
             return pairs.map { pair in
                 let initialAndFinal = pair.components(separatedBy: "-")
@@ -47,11 +49,11 @@ final class DayFourTests: XCTestCase {
                 } else { fatalError() }
             }
         }
-        
-        let result = ranges.map { range in
-            range[1].overlaps(range[0])
+    }
+    
+    private func result(from ranges: [[ClosedRange<Int>]], condition: ([ClosedRange<Int>]) -> (Bool)) -> [Bool] {
+        ranges.map { range in
+            condition(range)
         }.filter { $0 == true }
-        
-        XCTAssertEqual(result.count, 830)
     }
 }
